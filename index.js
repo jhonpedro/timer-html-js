05396904effe16993ee1242a234bff9f7c520c32
João Pedro <pedrobarros2010@gmail.com>
2021-05-02 21:07:59 -0300

    feat: creating jump input for seconds

--- index.js
+++ index.js
@@ -2,12 +2,17 @@
 	'use strict'
 
 	var $input = document.querySelector('[data-js="input"]')
+	var $jumpInput = document.querySelector('[data-js="input-jump"]')
 
 	var $startButton = document.querySelector('[data-js="button-start"]')
 	var $stopButton = document.querySelector('[data-js="button-stop"]')
 	var $resetButton = document.querySelector('[data-js="button-reset"]')
 
 	var timer
+
+	$jumpInput.addEventListener('keyup', (event) => {
+		$jumpInput.value = event.target.value
+	})
 
 	$startButton.addEventListener('click', () => {
 		timer = setInterval(() => {
@@ -20,7 +25,7 @@
 			minutes = +minutes
 			seconds = +seconds
 
-			seconds += 1
+			seconds += +$jumpInput.value ? +$jumpInput.value : 1
 
 			if (seconds >= 60) {
 				minutes += parseInt(seconds / 60)
@@ -28,11 +33,11 @@
 			}
 			if (minutes >= 60) {
 				hours += parseInt(minutes / 60)
-				seconds = parseInt(minutes % 60)
+				minutes = parseInt(minutes % 60)
 			}
 			if (hours >= 24) {
 				days += parseInt(hours / 24)
-				seconds = parseInt(hours % 60)
+				hours = parseInt(hours % 24)
 			}
 
 			$input.value = `${days >= 10 ? days : `0${days}`}:${
